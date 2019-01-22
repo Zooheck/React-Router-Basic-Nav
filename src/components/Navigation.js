20dec7e36b99b7f86934309afeddf25f024ce09a
Zooheck <davidscottflack@gmail.com>
2019-01-21 20:48:27 -0600

    Add navigation toggle functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link to={"/"}><a href="">Home</a></Link>
         </div>
         <div>
-          <a href="">About</a>
+          <Link to={"/about"}><a href="/about">About</a></Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link to={"/contact"}><a href="/contact">Contact</a></Link>
         </div>
       </div>
     </div>
